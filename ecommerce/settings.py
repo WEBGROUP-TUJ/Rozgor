14e9f761a26c2b7b27e42d3dc00b46fcb1e012db
UlugbekMuslitdinov <root@WIN-E8C12OAVHL9.localdomain>
2020-12-29 23:53:34 +0500

    at

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -179,7 +179,7 @@ STATICFILES_FINDERS = [ # new
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-AUTH_USER_MODEL = 'users.CustomUser' # new
+
 
 
 LOGIN_REDIRECT_URL = 'shop:category_list'
