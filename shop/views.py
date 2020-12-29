93915e20d0660f2c99e340a41c8b955465c57192
UlugbekMuslitdinov <root@WIN-E8C12OAVHL9.localdomain>
2020-12-29 13:16:24 +0500

    v12

--- shop/views.py
+++ shop/views.py
@@ -55,6 +55,8 @@ class CategoryListView(ListView):
     context_object_name = 'category_list'
     template_name = 'shop/product/list.html'
 
+nav_category = category.category_list.order_by('id')[:5]
+
 class CategoryDetailView(DetailView):
     model = Category
     context_object_name = 'category'
