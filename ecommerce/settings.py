cdf29fc61c7c50c538bc946e0a0c8f3977c51e6b
UlugbekMuslitdinov <root@WIN-E8C12OAVHL9.localdomain>
2021-01-01 19:12:18 +0500

    str

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('templates'))],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),]
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
