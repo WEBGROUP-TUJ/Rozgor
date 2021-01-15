d24cc9a425acd3f5cbee0aed42d7cb24f86d46f6
UlugbekMuslitdinov <root@WIN-E8C12OAVHL9.localdomain>
2021-01-15 20:59:19 +0500

    smtp_mailing

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,17 +145,18 @@ EMAIL_FILE_PATH - указывать путь где будут хранитьс
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' - выводит сообщение
 в консоль.
 '''
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = 'emails/email-messages/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL = 'ulugbekmuslitdinov@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ulugbekmuslitdinov@gmail.com'
+EMAIL_HOST_PASSWORD = 'u29062003'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
-MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-info',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
-}
+
 
 
 # Internationalization
