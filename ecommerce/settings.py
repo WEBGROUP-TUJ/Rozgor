047175be71a9b4138f90c33a5466f0972a18d6e3
UlugbekMuslitdinov <ulugbekmuslitdinov@gmail.com>
2021-02-10 23:04:25 +0500

    host change

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['fathomless-sea-68276.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fathomless-sea-68276.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
