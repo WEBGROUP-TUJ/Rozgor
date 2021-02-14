7b43e8377f7228fee23f87b0f565b24c76d32fd9
UlugbekMuslitdinov <ulugbekmuslitdinov@gmail.com>
2021-02-14 15:58:51 +0500

    translated form

--- orders/models.py
+++ orders/models.py
@@ -5,21 +5,21 @@ from shop.models import Product
 
 
 class Order (models.Model):
-    first_name = models.CharField("Имя", max_length=60)
-    last_name = models.CharField("Фамилия", max_length=60)
-    phone_number = models.CharField("Номер телефона", max_length=13, null=True)
-    email = models.EmailField("Электронная почта", null=True, blank=True)
-    address = models.CharField("Адрес", max_length=150)
-    postal_code = models.CharField("Почтовый индекс", max_length=30)
-    city = models.CharField("Город", max_length=100)
+    first_name = models.CharField("Ism", max_length=60)
+    last_name = models.CharField("Familiya", max_length=60)
+    phone_number = models.CharField("Telefon raqami", max_length=13, null=True)
+    email = models.EmailField("Elektron pochta", null=True, blank=True)
+    address = models.CharField("Manzil", max_length=150)
+    postal_code = models.CharField("Pochta indeksi", max_length=30)
+    city = models.CharField("Shahar", max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField("Оплачено", default=False)  # оплачен ли заказ
+    paid = models.BooleanField("To'langan", default=False)  # оплачен ли заказ
 
     class Meta:
         ordering = ('-created',)
-        verbose_name = 'заказ'
-        verbose_name_plural = 'заказы'
+        verbose_name = 'Buyurtma'
+        verbose_name_plural = 'Buyurtmalar'
 
     def __str__(self):
         return 'Заказ {}'.format(self.id)
@@ -32,8 +32,8 @@ class Order (models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
-    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField("Количество", default=1)
+    price = models.DecimalField("Narx", max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField("Ko'pligi", default=1)
 
     def __str__(self):
         return '{}'.format(self.id)
