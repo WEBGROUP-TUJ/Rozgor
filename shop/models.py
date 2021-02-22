5282a935be0d924a3db09f9029e5a9d56ebede9b
ulugbekmuslitdinov <ulugbekmuslitdinov@gmail.com>
2021-02-22 19:54:40 +0500

    imgs problem

--- shop/models.py
+++ shop/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=150, unique=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    cover = models.ImageField(upload_to=get_upload_path, null=True)
+    cover = models.CharField(max_length=200, null=True)
 
     class Meta:
         indexes = [
@@ -61,7 +61,7 @@ class Product(models.Model):
     stock = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to=get_upload_path, blank=True)
+    image = models.CharField(max_length=200, null=True)
 
     class Meta:
         ordering = ('name',)
