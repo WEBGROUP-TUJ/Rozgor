3d7c036495fd7dda942b7fd515116b110c2ed6ab
timakasimovkra <timakasimovkra@gmail.com>
2021-03-05 22:51:52 +0500

    breakpoints

--- static/js/script.js
+++ static/js/script.js
@@ -20,17 +20,22 @@ const swiper = new Swiper('.swiper-container', {
     },
 
     breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20
+        601: {
+            slidesPerView: 2,
+            spaceBetween: 5
         },
-        // when window width is >= 480px
-        480: {
-            slidesPerView: 1,
-            spaceBetween: 30
+        1001: {
+            slidesPerView: 3,
+            spaceBetween: 5
         },
-        // when window width is >= 640px
+        1401: {
+            slidesPerView: 4,
+            spaceBetween: 5
+        },
+        1601: {
+            slidesPerView: 5,
+            spaceBetween: 0
+        },
         1801: {
             slidesPerView: 6,
             spaceBetween: 0
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -20,21 +20,26 @@ const swiper = new Swiper('.swiper-container', {
     },
   
     breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 30
-      },
-      // when window width is >= 640px
-      1801: {
-        slidesPerView: 6,
-        spaceBetween: 0
-      }
+        601: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        1001: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1401: {
+            slidesPerView: 4,
+            spaceBetween: 5
+        },
+        1601: {
+            slidesPerView: 5,
+            spaceBetween: 0
+        },
+        1801: {
+            slidesPerView: 6,
+            spaceBetween: 0
+        }
     }
   });
 
